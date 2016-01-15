3b18ab78d1134947bb258d28da0ab7784234ba1f
amvaleh <amvaleh@gmail.com>
2016-01-15 12:38:22 +0330

    assets changed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( admin.css )
 Rails.application.config.assets.precompile += %w( admin.js )
 
+Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/
+
+# Fonts
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
