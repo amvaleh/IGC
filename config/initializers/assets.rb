73ac53fb9753635f1fe5f6d1652e0de6f3612f41
amvaleh <amvaleh@gmail.com>
2016-01-15 13:21:30 +0330

    damn borin sass error wasted 3 hours of life

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,3 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( admin.css )
 Rails.application.config.assets.precompile += %w( admin.js )
 
-Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/
-
-# Fonts
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
