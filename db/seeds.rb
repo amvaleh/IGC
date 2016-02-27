0bfa54158e3d5a396d6c499586627ab0b8153782
amvaleh <amvaleh@gmail.com>
2016-02-27 12:21:40 +0330

    header langs removed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ConstructionEquipmentCategory.delete_all
+ConstructionEquipmentCategory.create!(id: 1, name:"Crawler Crane")
+ConstructionEquipmentCategory.create!(id: 2, name:"Other Crane")
+ConstructionEquipmentCategory.create!(id: 3, name:"Truck")
+ConstructionEquipmentCategory.create!(id: 4, name:"Air Compressor")
+ConstructionEquipmentCategory.create!(id: 5, name:"Transportation")
+ConstructionEquipmentCategory.create!(id: 6, name:"Civil & Road Machinary")
+ConstructionEquipmentCategory.create!(id: 7, name:"Tools")
